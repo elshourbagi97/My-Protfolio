c01b3b8e0bf8e5c07668e8793a724d536f260274
Adham Elshourbagi <elshourbagi97@gmail.com>
2025-07-04 23:09:14 +0300

    last Updating

--- src/About/about.jsx
+++ src/About/about.jsx
@@ -40,11 +40,9 @@ export default function About() {
         <h3>About me</h3>
         <div className="aboutMain">
           <h4>I am a Full-Stack developer based in Egypt. Has Engineering background.</h4>
-          <p>I’m Adham Elshourbagy, a 27-year-old Full Stack Web Developer based in Egypt, with a background in Mechanical Engineering from Cairo University.</p>
-          <p>While my academic journey started in engineering, my real passion led me to tech. I completed a one-year Full Stack Development diploma at AMIT Academy, graduating with a 95% grade — and ever since, I’ve been building dynamic, responsive, and user-friendly web applications.</p>
+          <p>I'm Adham Elshourbagy, a 27-year-old Full Stack Web Developer from Egypt with a background in Engineering from Cairo University. I shifted to tech after completing a Full Stack Development diploma at AMIT Academy with a 95% grade..</p>
+          <p>I build dynamic, user-friendly apps using html, scss, js, css, React, Laravel, Node.js, and MySQL. I’m passionate about clean code, UI/UX, and responsive design. Currently, I’m exploring Webflow and improving my design skill</p>
           <p>During the program, I gained hands-on experience in both frontend and backend development. On the frontend, I’ve worked with technologies like HTML, CSS, JavaScript, React, and responsive design to build clean and engaging user interfaces. On the backend, I developed solid skills using Node.js, Express, Laravel, MySQL, and APIs, allowing me to build robust, full-featured applications from the ground up.</p>
-          <p>I’m especially interested in technologies like React, Node.js, and Laravel, and I care deeply about clean code, accessibility, and creating smooth user experiences. Right now, I’m exploring Webflow, diving deeper into UI/UX design, and always looking to push my skill set further.</p>
-          <p>Outside of coding, you’ll usually find me on the football field, behind a camera, or teaming up in a game of Valorant. I'm passionate, curious, and excited to keep growing in this ever-evolving world of web development.</p>
           <a className="btn btn-warning" href="/AdhamElShourbagy-CV.pdf" download title="Download Cv">
             Download MY CV
           </a>
